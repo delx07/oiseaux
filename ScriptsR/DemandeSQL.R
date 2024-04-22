5ace97414d61b522362287bfe86fa4db888e6042
ThomasCournoyer <164022132+ThomasCournoyer@users.noreply.github.com>
2024-04-22 15:50:04 -0400

    correction des demande pour sélectionner les passériformatus

--- ScriptsR/DemandeSQL.R
+++ ScriptsR/DemandeSQL.R
@@ -20,15 +20,19 @@ Demande_SQL=function(liste_table){
   tableSQL_Fig2=tableSQL_Fig1
   tableSQL_Fig2$lat=as.integer(tableSQL_Fig1$lat)
   
-  tableSQL_Fig3=dbGetQuery(oiseaux_bd, "SELECT lat, valid_scientific_name, count(valid_scientific_name) AS abondance
+  tableSQL_Fig3=dbGetQuery(oiseaux_bd, "SELECT lat, ordre, count(ordre) AS abondance
              FROM principale
-             LEFT JOIN site ON principale.site_id = site.site_id 
-             GROUP BY valid_scientific_name, lat  " )
+             LEFT JOIN site ON principale.site_id = site.site_id
+             LEFT JOIN taxonomie ON principale.valid_scientific_name = taxonomie.valid_scientific_name
+             WHERE ordre in ('Passeriformes')
+             GROUP BY ordre, lat  " )
   
-  tableSQL_Fig4=dbGetQuery(oiseaux_bd, "SELECT CONCAT(strftime('%Y', date_obs),'-' ,strftime('%m', date_obs)) , valid_scientific_name, COUNT(valid_scientific_name) AS abondance 
+  tableSQL_Fig4=dbGetQuery(oiseaux_bd, "SELECT CONCAT(strftime('%Y', date_obs),'-' ,strftime('%m', date_obs)) , ordre, COUNT(ordre) AS abondance 
               FROM principale
               LEFT JOIN temps ON principale.id_date = temps.id_date
-              GROUP BY valid_scientific_name, strftime('%Y', date_obs),strftime('%m', date_obs)" )
+              LEFT JOIN taxonomie ON principale.valid_scientific_name = taxonomie.valid_scientific_name
+              WHERE ordre in ('Passeriformes')
+              GROUP BY ordre, strftime('%Y', date_obs),strftime('%m', date_obs)" )
   
   liste_table_SQL=list(tableSQL_Fig1, tableSQL_Fig2, tableSQL_Fig3, tableSQL_Fig4)
   #Nous déconnecte de la base de données oiseaux.db pour permettre 
